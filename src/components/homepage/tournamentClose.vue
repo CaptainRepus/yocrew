46cbb56a91ce9f0c3e0a56b190b77c91cb9ece81
CaptainRepus <lazorik.leonard@outloo.com>
2024-05-14 20:20:04 +0200

    detail

--- src/components/homepage/tournamentClose.vue
+++ src/components/homepage/tournamentClose.vue
@@ -3,21 +3,23 @@
       <h1 class="p-3 pb-0 font-bold text-2xl max-md:mb-2 max-md:ps-0">Naše akcie a turnaje</h1>
       <div class="h-full w-full p-3 max-md:p-0">
         <div class="flex flex-col gap-3 justify-center overflow-hidden h-[34.2rem] max-md:h-72">
-            <a v-if="tournamentStore.tournament.length > 0" :href="sortedTournaments[0].link" class="h-1/3 w-full px-3 rounded-xl border border-slate-700 bg-slate-800 overflow-hidden hover:bg-slate-600">
-                <div class="h-1/4 w-full flex items-end">
-                  <h1 class="font-bold text-2xl z-4">{{ sortedTournaments[0].tournament }}</h1>
-                </div>
-                <div class="h-1/3 w-full flex flex-col">
-                  <h3 class="text-lg font-semibold pb-1 z-4">{{ sortedTournaments[0].game }}</h3>
-                  <h3 class="text-lg font-semibold pb-1 z-4">{{ sortedTournaments[0].date }}</h3>
-                  <p class="text-sm">{{ sortedTournaments[0].description }}</p>
-                </div>
-            </a>
+          <a v-if="tournamentStore.tournament.length > 0" :href="sortedTournaments[0].link" class="relative h-1/3 w-full px-3 rounded-xl border border-slate-700 bg-slate-800 overflow-hidden hover:bg-slate-600">
+            <ion-icon aria-hidden="true" class="absolute opacity-30 text-[20rem] text-green-500 z-10 top-[-1rem] right-[-2rem]" :icon="gameController" />
+            <div class="h-1/4 w-full flex items-end">
+              <h1 class="font-bold text-2xl z-20">{{ sortedTournaments[0].tournament }}</h1>
+            </div>
+            <div class="h-1/3 w-full flex flex-col">
+              <h3 class="text-lg font-semibold pb-1 z-20">{{ sortedTournaments[0].game }}</h3>
+              <h3 class="text-lg font-semibold pb-1 z-20">{{ sortedTournaments[0].date }}</h3>
+              <p class="text-sm z-20">{{ sortedTournaments[0].description }}</p>
+            </div>
+          </a>
           <div v-else class="h-1/3 w-full bg-gray-500">
             Loading...
           </div>
-
-          <a v-if="tournamentStore.tournament.length > 0" :href="sortedTournaments[1].link" class="h-1/3 w-full px-3 rounded-xl bg-slate-800 border border-slate-700 overflow-hidden hover:bg-slate-600">
+  
+          <a v-if="tournamentStore.tournament.length > 0" :href="sortedTournaments[1].link" class="relative h-1/3 w-full px-3 rounded-xl bg-slate-800 border border-slate-700 overflow-hidden hover:bg-slate-600">
+            <ion-icon aria-hidden="true" class="absolute opacity-30 text-[20rem] text-green-500 z-10 top-[-1rem] right-[-2rem]" :icon="calendar" />
             <div class="h-1/4 w-full flex items-end">
               <h1 class="font-bold text-2xl">{{ sortedTournaments[1].tournament }}</h1>
             </div>
@@ -30,8 +32,9 @@
           <div v-else class="h-1/3 w-full bg-gray-500">
             Loading...
           </div>
-
-          <a v-if="tournamentStore.tournament.length > 0" :href="sortedTournaments[2].link" class="h-1/3 w-full px-3 rounded-xl bg-slate-800 border border-slate-700 overflow-hidden hover:bg-slate-600">
+  
+          <a v-if="tournamentStore.tournament.length > 0" :href="sortedTournaments[2].link" class="relative h-1/3 w-full px-3 rounded-xl bg-slate-800 border border-slate-700 overflow-hidden hover:bg-slate-600">
+            <ion-icon aria-hidden="true" class="absolute opacity-30 text-[20rem] text-green-500 z-10 top-[-1rem] right-[-2rem]" :icon="calendar" />
             <div class="h-1/4 w-full flex items-end">
               <h1 class="font-bold text-2xl">{{ sortedTournaments[2].tournament }}</h1>
             </div>
@@ -44,7 +47,6 @@
           <div v-else class="h-1/3 w-full bg-gray-500">
             Loading...
           </div>
-
         </div>
       </div>
     </div>
@@ -53,7 +55,7 @@
   <script>
   import { ref, computed } from 'vue';
   import { useTournamentStore } from '@/store/useTournamentStore.js';
-  import { gameController } from 'ionicons/icons';
+  import { gameController, calendar } from 'ionicons/icons';
   
   export default {
     setup() {
@@ -72,7 +74,8 @@
       return {
         tournamentStore,
         sortedTournaments,
-        gameController
+        gameController,
+        calendar
       };
     },
   };
